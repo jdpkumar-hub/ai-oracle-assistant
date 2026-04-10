ac7aba4a88c3e7c2849c163e448365de33ad9dc2
jdpkumar-hub <jdpkumar@gmail.com>
2026-04-09 20:34:04 -0500

    Add files via upload

---
+++ ai-auth-frontend/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  "https://wequqsbvhydvugifevhm.supabase.co",
+  "sb_publishable_ZOfGu0PLriJqtJLdmk6Bkg_mJ3HrURB"
+)
